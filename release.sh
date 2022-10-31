19895fcc849d8d6a2e5e905d8476f71d57bbe647
Saagar Jha <saagar@saagarjha.com>
2022-10-31 01:33:01 -0700

    Add a script to build a new release

---
+++ release.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+set -x
+
+swiftc -O -parse-as-library -target x86_64-apple-macosx12.0 unxip.swift -o unxip-x86_64
+swiftc -O -parse-as-library -target arm64-apple-macosx12.0 unxip.swift -o unxip-arm64
+lipo -create unxip-arm64 unxip-x86_64 -output unxip
+codesign -s "Developer ID" --options=runtime --timestamp unxip
+zip unxip.zip unxip
+xcrun notarytool submit unxip.zip --keychain-profile "AC_PASSWORD" --wait
