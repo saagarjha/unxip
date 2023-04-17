8f43e89ac74b301dd70fad160e620a4a801f7584
Saagar Jha <saagar@saagarjha.com>
2023-04-17 05:13:44 -0700

    Enable whole module optimization for binaries

--- release.sh
+++ release.sh
@@ -2,8 +2,8 @@
 
 set -x
 
-swiftc -O -parse-as-library -target x86_64-apple-macosx12.0 unxip.swift -o unxip-x86_64
-swiftc -O -parse-as-library -target arm64-apple-macosx12.0 unxip.swift -o unxip-arm64
+swiftc -O -whole-module-optimization -parse-as-library -target x86_64-apple-macosx12.0 unxip.swift -o unxip-x86_64
+swiftc -O -whole-module-optimization -parse-as-library -target arm64-apple-macosx12.0 unxip.swift -o unxip-arm64
 lipo -create unxip-arm64 unxip-x86_64 -output unxip
 codesign -s "Developer ID" --options=runtime --timestamp unxip
 zip unxip.zip unxip
